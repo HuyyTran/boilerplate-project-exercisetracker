8d5f46faef8fc76ffe92b161247d1badfae1434c
HuyyTran <huytrn.dev@gmail.com>
2024-02-22 21:50:33 +0700

    update npm

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+// connect to db
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+
+//middleware
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+}, bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
+// create a 'User' Model
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  userName: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
 
 
+app.post('/api/users', (req, res) => {
 
+});
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
