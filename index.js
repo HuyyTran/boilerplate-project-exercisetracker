b451029323e01dc8a5af699de9c0b0eed8cb87dc
HuyyTran <huytrn.dev@gmail.com>
2024-02-23 11:19:26 +0700

    finish handle Post to /api/users/L_id/exercises

--- index.js
+++ index.js
@@ -20,15 +20,19 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// create a 'User' Model
 const Schema = mongoose.Schema;
+// create a 'Exercise' Model
+const exerciseSchema = new Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+});
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+// create a 'User' Model
 const userSchema = new Schema({
-  username: String,
-  description: String,
-  duration: Number,
-  date: Date,
-  count: Number,
-  log: [{ description: String, duration: Number, date: Date }],
+  username: { type: String, required: true },
+  log: [exerciseSchema],
 });
 let User = mongoose.model("User", userSchema);
 
@@ -68,50 +72,47 @@ app.get("/api/users", (req, res) => {
 });
 
 //handle Post to /api/users/L_id/exercises
-app.post("/api/users/:_id/exercises", (req, res) => {
-  let id = req.params._id;
-  let description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date;
-  console.log(id, description, duration, date);
+app.post(
+  "/api/users/:_id/exercises",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    let id = req.params._id;
+    let description = req.body.description;
+    let duration = parseInt(req.body.duration);
+    let date = req.body.date;
+    console.log(id, description, duration, date);
 
-  //if date is empty
-  if (date == "") {
-    date = new Date();
-  }
-  //   User.findOneAndUpdate(
-  //     { _id: id },
-  //     { description: description, duration: duration, date: date }
-  //   )
-  //     .then((data) => {
-  //       res.json({
-  //         _id: data._id,
-  //         username: data.username,
-  //         date: data.date.toDateString(),
-  //         duration: data.duration,
-  //         description: data.description,
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       if (err) return console.error(err);
-  //     });
-  // });
-  User.findOne(
-    { _id: id },
-  )
-    .then((data) => {
-      res.json({
-        _id: id,
-        username: data.username,
-        date: date.toDateString(),
-        duration: duration,
-        description: description,
-      });
-    })
-    .catch((err) => {
-      if (err) return console.error(err);
+    console.log("after modify: ", id, description, duration, date);
+    let newExercise = new Exercise({
+      description: req.body.description,
+      duration: parseInt(req.body.duration),
+      date: req.body.date,
     });
-});
+
+    if (newExercise.date == "") {
+      newExercise.date = new Date().toISOString().substring(0, 10);
+    }
+    User.findByIdAndUpdate(
+      id,
+      {
+        $push: { log: newExercise },
+      },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        let resObject = {};
+        resObject["_id"] = updatedUser.id;
+        resObject["username"] = updatedUser.username;
+        resObject["date"] = new Date(newExercise.date).toDateString();
+        resObject["duration"] = newExercise.duration;
+        resObject["description"] = newExercise.description;
+        res.json(resObject);
+      })
+      .catch((err) => {
+        if (err) return console.error(err);
+      });
+  }
+);
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
